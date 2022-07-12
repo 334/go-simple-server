70c236c8ff1978bf072e842715cf957a7f098088
Jai Gupta <jn@Jais-MacBook-Pro.local>
2022-07-12 17:14:12 +0530

    commit2

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
 FROM golang:latest
-
-
 RUN mkdir /build
 WORKDIR /build
-
 RUN export GO111MODULE=on
-
-ADD ./* /build
-
-RUN cd /build
-
+RUN cd /build && git clone git@github.com:334/go-simple-server.git
 RUN go build 
 
 EXPOSE 8000
 
-ENTRYPOINT [ "/build/web-app" ]
+ENTRYPOINT [ "/build/goDir/web-app" ]
--- goWebApp.go
+++ goWebApp.go
@@ -1,19 +1,17 @@
 package main
 
-import("fmt"
- "net/http")
+import (
+	"fmt"
+	"net/http"
+)
 
+func index_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Go")
+}
 
- func index_handler(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello Go")
- }
+func main() {
 
- func main(){
+	http.HandleFunc("/", index_handler)
+	http.ListenAndServe(":8000", nil)
 
-    http.HandleFunc("/", index_handler)
-    http.ListenAndServe(":8000", nil)
-
- }
-
-
-
+}
